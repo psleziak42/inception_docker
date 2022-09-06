1581160e2acf3a4dd20062ad627302f9d5b3936c
Przemyslaw Sleziak <przemyslaw.sleziak@criticaltechworks.com>
2022-09-06 15:27:53 +0100

    mariadb_entrypoint_backup is now pretty and ready to go.

--- srcs/requirements/mariadb/tools/mariadb_entrypoint_backup.sh
+++ srcs/requirements/mariadb/tools/mariadb_entrypoint_backup.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Why we create "admin" user?
-# Root user is sort of admin, but it is not password but socket identified.
-# Dunno what it exactly means but having admin with password provides easier
-# access to the DB
-
-#to bylo w moim wordpress_entrypoint
-#/etc/mysql/mariadb.conf.d/50-server.cnf
-
 # SOME COMMANDS:
 # - SELECT USER()/CURRENT_USER(); - whoami
 # - SELECT User FROM mysql.user; - shows all users
@@ -21,7 +13,6 @@
 
 # Exits script if one of the commands fail
 set -e
-service mysql start;
 
 sed -i "s/^bind-address/#bind-address/" /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -34,40 +25,53 @@ sed -i 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 # This command wont be necessary as we are already inside the database i guess
 # mariadb --user=root -p
 
-#loggin in to database
-
-
 # Create new Database for Wordpress if it does not exist yet
 if [ ! -d /var/lib/mysql/wordpressDB ]
 then
-	mysql -e "CREATE DATABASE wordpressDB;"
+  # We have to start mysql service in order to be albe to connect to it and make changes
+  service mysql start;
 
-# NOT SURE IF NECESSARY:
-# USE [database name]; - to make database active
+  mysql -e "CREATE DATABASE wordpressDB;"
+  #  delete anonymous user that allows to access database without the password
+  #  it is good to have during testing (that means setup) but for production
+  #  (that means when we put it to the internet as ready) it should be deleted
+  #  for safety.
+  mysql -e "DELETE FROM mysql.user WHERE User='';"
+  #  removing test database
+  mysql -e "DROP DATABASE IF EXISTS test;"
+  mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 
-#mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
+  #  % means we can login as root from any IP address
+  # "GRANT ALL [PRIVILEGES is optional] ON
+  # "WITH GRANT OPTION" - means that this user can grant privileges to other user at the given privilege level
+  mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
 
-# Create Admin User
-# CREATE USER 'superuser'@localhost identified by 'tightconnector'
-# GRANT ALL PRIVILEGES ON *.* TO 'superuser'@localhost
-#lub
-# that part '172.18.0.2/16' works with % but i try to see if other options work also (or change % or try nginx_)
-mysql -e "GRANT ALL ON *.* TO 'superuser'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;"
-mysql -e "FLUSH PRIVILEGES;"
-# "GRANT ALL [PRIVILEGES is optional] ON - word privileges is optional
-# "WITH GRANT OPTION" - means that this user can grant privileges to other user at the given privilege level
+  # Create Regular User (only access to wordpressdb?)
+  # IF NOT EXISTS - prevents us to create user if it exists and from errors
+  # Currently as if [ ! -d ... ] serves as protector it is not necesary but
+  # I am leaving it as in the future I may have better blueprint
+  mysql -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' identified by 'wp';"
+  mysql -e "GRANT ALL PRIVILEGES ON wordpressDB.* TO 'wordpress'@'%';"
+  #lub
+  #GRANT ALL ON wordpressDB.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wp' WITH GRANT OPTION;
+  mysql -e "FLUSH PRIVILEGES;"
 
-# Create Regular User (only access to wordpressdb?)
-mysql -e "CREATE USER IF NOT EXISTS 'wordpress'@'%' identified by 'wp';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpressDB.* TO 'wordpress'@'%';"
-#lub
-#GRANT ALL ON wordpressDB.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wp' WITH GRANT OPTION;
-mysql -e "FLUSH PRIVILEGES;"
-
-#mysql -u superuser -ppassword -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'buseta';"
-
-#mysqladmin -u root -pbuseta shutdown;
-service mysql stop;
+  # We must stop the service because later in Dockerfile there is command "mysqld"
+  # that runs mysql in foreground mode
+  service mysql stop;
 fi
 
+# !!!
+# $0 - script itself
+# $1 - 1st argument, $2 - 2nd argument (doesnt apply here)
+# $@ - refers to all script's arguments aka command line arguments.
+# mariadb_entrypoint.sh mysqld_safe - mysqld_safe is an argument to that script
+# The way Docker works is that ENTRYPOINT is a script and CMD are arguments to that scritp.
+# exec - takes over current process (we frequently use fork with exec inside pogram
+#         because exec execute given command and kills itself. In that case it kills
+#         forked process so after execution our program may still run) and quits.
+#         But in order our contaier to be alive it cant quit, it must stay alive.
+#         This is the reason why EVERY DOCKER CONTAINER MUST RUN blocking FOREGROUND cmd.
+#         Othewise CMD will be executed (imagine ls -l) and container will exit!!
+# Exec $@ says now execute all of the arguments that are present in CMD
 exec "$@"
