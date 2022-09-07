51bfc9755012ef485115249f979973ca4577a1cb
Przemyslaw Sleziak <przemyslaw.sleziak@criticaltechworks.com>
2022-09-07 12:56:34 +0100

    wp Dockerfile ready, mariadb after changes also works.  going for lunch

--- srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
+++ srcs/requirements/wordpress/tools/wordpress_entrypoint.sh
@@ -3,12 +3,11 @@
 #cant this be inside if?
 sed -ie "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/" /etc/php/7.3/fpm/pool.d/www.conf
 
-ls -l
 if [ ! -f "/var/www/html/wp-config.php" ]
 then
   # remove wp-config-sample
   rm /wordpress/wp-config-sample.php
-  ls -l
+
   #this is failing because /var/www/html doesnt exist yet (we could mkdir) if we call this from inside
   #dockerfile. Entrypoint script is the first thing called on running container!
   mv /wordpress/* /var/www/html/
